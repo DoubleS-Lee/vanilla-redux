079ecb033a3b51920615ba01bdb15836a50ebb59
DoubleS-Lee <lss8825@gmail.com>
2021-02-15 15:04:47 +0900

    1.4:코드개선

--- src/index.js
+++ src/index.js
@@ -11,21 +11,22 @@ number.innerText = 0;
 // 외부에서 reducer에게 데이터를 전달해줘야해서 action을 reducer로 보내야함
 // 이때 dispatch를 이용해서 외부에서 reducer의 action으로 지령을 보낸다
 
+const ADD = "ADD";
+const MINUS = "MINUS";
 
 
 // reducer
 // 여기서 count는 state라고 생각해도 된다
 const countModifier= (count = 0, action) => {
-  if (action.type === "ADD") {
-    count = count + 1;
-  } else if (action.type === "MINUS") {
-    count = count - 1;
-  } else {
-    console.log("입력값을 확인해주세요")
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
-  console.log(count, action);
-  return count;
-}
+};
 
 // store
 // countStore는 store의 역할을 하고 있음
@@ -52,11 +53,11 @@ countStore.subscribe(onChange);
 // dispatch
 // action으로 데이터를 보냄(object 형태로)
 const handleAdd = () => {
-  countStore.dispatch({type:"ADD"});
+  countStore.dispatch({type:ADD});
 };
 
 const handleMinus = () => {
-  countStore.dispatch({type:"MINUS"});
+  countStore.dispatch({type:MINUS});
 };
 
 add.addEventListener('click', handleAdd);
