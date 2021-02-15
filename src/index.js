acf6c961315a557c80b5c5b9322ff9f692e8ea5e
DoubleS-Lee <lss8825@gmail.com>
2021-02-15 14:06:08 +0900

    1.1:createStore, reducer 사용

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
+import { createStore } from 'redux';
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerText = count;
+const countModifier= (count = 0) => {
+  return count;
+}
 
-const updateText = () => {
-  number.innerText = count;
-};
+// countStore는 store의 역할을 하고 있음
+// countModifier는 reducer 함수의 역할을 하고 있음
+const countStore = createStore(countModifier);
 
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-};
-
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+// reducer에서 return으로 넘겨준 값을 보려면 .getState()를 사용해야 함
+console.log(countStore.getState());
