4e6938591fa7ad363066a42fd49039abb5f5d388
DoubleS-Lee <lss8825@gmail.com>
2021-02-15 14:55:37 +0900

    1.3:subscribe 함수에 대해

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
 
 
 // reducer에서 data를 수정하고
@@ -31,14 +32,32 @@ const countModifier= (count = 0, action) => {
 // countModifier는 reducer 함수의 역할을 하고 있음
 const countStore = createStore(countModifier);
 
+// store를 console.log 해보면 4가지 함수가 나온다(dispatch, subscribe, getState, replaceReducer)
+console.log(countStore);
+
 // reducer에서 return으로 넘겨준 값을 보려면 .getState()를 사용해야 함
 // console.log(countStore.getState());
 
-// action으로 데이터를 보냄(object 형태로)
-countStore.dispatch({type:"ADD"})
-countStore.dispatch({type:"ADD"})
-countStore.dispatch({type:"ADD"})
-countStore.dispatch({type:"ADD"})
-countStore.dispatch({type:"ADD"})
-countStore.dispatch({type:"MINUS"})
+// subscribe
+// subscribe : store에게 변화가 있을때마다 실행하는 함수이다
+// store의 count 값을 받아서 number.innerText에게 업데이트를 해준다 
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
 
+// countStore에게 변화가 있을때마다 onChange 함수를 실행한다
+countStore.subscribe(onChange);
+
+
+// dispatch
+// action으로 데이터를 보냄(object 형태로)
+const handleAdd = () => {
+  countStore.dispatch({type:"ADD"});
+};
+
+const handleMinus = () => {
+  countStore.dispatch({type:"MINUS"});
+};
+
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
